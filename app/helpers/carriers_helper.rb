e09915ef95b61e3f7bd3cff2fe08c33fb9c2489d
PatronPj <pujan.chamani.cheri@gmx.de>
2018-10-27 13:02:59 +0200

    carrier show and index views

--- app/helpers/carriers_helper.rb
+++ app/helpers/carriers_helper.rb
@@ -1,2 +1,6 @@
 module CarriersHelper
+
+  def carrier_author(carrier)
+    user_signed_in? && current_user.id == carrier.user_id
+  end
 end
--- app/models/carrier.rb
+++ app/models/carrier.rb
@@ -6,6 +6,6 @@ class Carrier < ApplicationRecord
 
   validates :surname, :lastname, :price, presence: true
   validates :description, length: { maximum: 1000, too_long: "%{count} characters is the maximum allowed." }
-  validates :price, numericality: { only_integer: true }, length: { maximum: 7 }
+  validates :price, length: { maximum: 7 }
 
 end
