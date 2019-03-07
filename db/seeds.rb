05afe0b0e1e8e66a657f7b245a1461d129ad62fc
PatronPj <pujan.chamani.cheri@gmx.de>
2019-03-07 15:00:11 +0100

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,45 +6,123 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-#user = User.new(
-#  id: 3,
-#  name: "Pujan",
-#  email: "pujan@admin.com",
-#  password: "admin1234",
-#  password_confirmation: "admin1234"
-#)
-#user.save!
+userMario = User.new(
+  id: 20,
+  name: "Mario",
+  email: "mario@test.com",
+  password: "test1234!",
+  password_confirmation: "test1234!"
+)
+userMario.save!
 
-#Carrier.create!([{
-#  surname: "Mike",
-#  lastname: "Muskelmann",
-#  description: "Ich bin sehr zuverlässig und vertrauenswürdig. Ihre Pakete würde ich ratzfratz von A nach B bringen!",
-#  price: "20",
-#  image: Rails.root.join("app/assets/images/carrier.jpg").open,
-#  user_id: user.id
-#},
-#{
-#  surname: "Fritz",
-#  lastname: "Feuer",
-#  description: "Ich transportiere Ihre Pakete gerne, melden Sie sich.",
-#  price: "15",
-#  image: Rails.root.join("app/assets/images/carrier.jpg").open,
-#  user_id: user.id
-#},
-#{
-#  surname: "Manfred",
-#  lastname: "Maler",
-#  description: "Moin Moin, ich bin 50 Jahre alt und würde Ihre Pakete liebendgern transportieren.",
-#  price: "10",
-#  image: Rails.root.join("app/assets/images/carrier.jpg").open,
-#  user_id: user.id
-#},
-#{
-#  surname: "Paul",
-#  lastname: "Protz",
-#  description: "Melden Sie sich einfach.",
-#  price: "25",
-#  image: Rails.root.join("app/assets/images/carrier.jpg").open,
-#  user_id: user.id
-#}
-#])
+userLuigi = User.new(
+  id: 21,
+  name: "Luigi",
+  email: "luigi@test.com",
+  password: "test1234!",
+  password_confirmation: "test1234!"
+)
+userLuigi.save!
+
+userWario = User.new(
+  id: 22,
+  name: "Wario",
+  email: "wario@test.com",
+  password: "test1234!",
+  password_confirmation: "test1234!"
+)
+userWario.save!
+
+userTodd = User.new(
+  id: 23,
+  name: "Todd",
+  email: "todd@test.com",
+  password: "test1234!",
+  password_confirmation: "test1234!"
+)
+userTodd.save!
+
+userPeach = User.new(
+  id: 24,
+  name: "Peach",
+  email: "peach@test.com",
+  password: "test1234!",
+  password_confirmation: "test1234!"
+)
+userPeach.save!
+
+userDonkey = User.new(
+  id: 25,
+  name: "Donkey",
+  email: "donkey@test.com",
+  password: "test1234!",
+  password_confirmation: "test1234!"
+)
+userDonkey.save!
+
+userBrowser = User.new(
+  id: 26,
+  name: "Donkey",
+  email: "browser@test.com",
+  password: "test1234!",
+  password_confirmation: "test1234!"
+)
+userBrowser.save!
+
+Carrier.create!([{
+  surname: "Mario",
+  lastname: "der aller Echte",
+  description: "Ich bin sehr zuverlässig und vertrauenswürdig. Ihre Pakete würde ich ratzfratz von A nach B bringen!",
+  price: "20",
+  image: Rails.root.join("app/assets/images/mario.png").open,
+  user_id: userMario.id
+},
+{
+  surname: "Luigi",
+  lastname: "Salvatore",
+  description: "Ich transportiere Ihre Pakete gerne, melden Sie sich.",
+  price: "15",
+  image: Rails.root.join("app/assets/images/luigi.png").open,
+  user_id: userLuigi.id
+},
+{
+  surname: "Wario",
+  lastname: "der Böse",
+  description: "Moin Moin, ich bin 50 Jahre alt und würde Ihre Pakete liebendgern transportieren.",
+  price: "10",
+  image: Rails.root.join("app/assets/images/wario.png").open,
+  user_id: userWario.id
+},
+{
+  surname: "Todd",
+  lastname: "Pilz",
+  description: "Melden Sie sich einfach.",
+  price: "25",
+  image: Rails.root.join("app/assets/images/todd.png").open,
+  user_id: userTodd.id
+},
+{
+  surname: "Peach",
+  lastname: "die Hübsche",
+  description: "Heeeeey, ich bin süße 23 Jahre alt und würde Ihre Pakete liebendgern transportieren.",
+  price: "10",
+  image: Rails.root.join("app/assets/images/peach.png").open,
+  user_id: userPeach.id
+},
+{
+  surname: "Donkey",
+  lastname: "Kong",
+  description: "Moin Moin, ich bin super schnell und würde Ihre Pakete liebendgern transportieren.",
+  price: "10",
+  image: Rails.root.join("app/assets/images/donkey.png").open,
+  user_id: userDonkey.id
+},
+{
+  surname: "Browser",
+  lastname: "der Endgegner",
+  description: "Moin Moin, ich bin 50 Jahre alt und würde Ihre Pakete liebendgern transportieren.",
+  price: "10",
+  image: Rails.root.join("app/assets/images/browser.png").open,
+  user_id: userBrowser.id
+}
+])
